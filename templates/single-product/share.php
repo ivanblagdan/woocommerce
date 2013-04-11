2035a8e91aee48990a72bbe7b0c91077e1d34493
Ivan <ivan@42.com.hr>
2013-04-11 18:19:00 +0200

    croatian translation

--- templates/single-product/share.php
+++ templates/single-product/share.php
@@ -12,5 +12,6 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 ?>
-
-<?php do_action('woocommerce_share'); // Sharing plugins can hook into here ?>
+<div class="share">
+	<?php do_action('woocommerce_share'); // Sharing plugins can hook into here ?>
+</div>
